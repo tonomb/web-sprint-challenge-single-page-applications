2f0eea8649d74ebc201f63c2629bf27aae606abe
antonio martinez baez <tono.mtzb@gmail.com>
2020-06-19 10:46:43 -0500

    home page  styled

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Navbar from './navbar'
 
+import './home.css'
+
 function Home(props){
 
 
     return(
         <div>
             <Navbar />
+            <div className='home-splash'>
+                <h1>Your favorite food, deliverd while coding</h1>
+                <button>Pizza?</button>
+            </div>
         </div>
     )
 }
